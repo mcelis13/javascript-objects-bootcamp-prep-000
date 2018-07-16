7a65a05a5598f6703975e5945158ab35636fb68d
Madeline Celis <madelinebcelis@gmail.com>
2018-07-16 15:49:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,9 @@ var playlist = {[artist]: songTitle};
 function updatePlayList(obj, artist, songTitle){
   obj[artist] = songTitle;
   return obj;
+};
+
+function removeFromPlayList(obj, artist){
+  delete obj[artist];
+  return obj;
 }
